78dea7deeb49bf481ca92b5989621a2801bd09cb
Hari <andrianarivohari@gmail.com>
2023-11-15 22:21:34 +0200

    Claudie

--- app/Views/template-parts/bannerhome.php
+++ app/Views/template-parts/bannerhome.php
@@ -5,10 +5,14 @@
 <div class="teamcontainer">
     <div class="ourteam">
         <h2>Notre équipe</h2>
-        <p>Notre chaleureuse équipe est ravie de vous accueillir dans ses locaux !</p>
+        <p>Nous sommes ravis de vous accueillir dans notre communauté dévouée d'assistantes maternelles agréées. Chez nous, votre enfant trouvera un environnement chaleureux, sûr et stimulant pour grandir et s'épanouir.
+        <br /><br />
+        Notre engagement envers le bien-être de chaque enfant est notre priorité absolue. 
+        Nous sommes impatientes de vous accueillir dans notre communauté chaleureuse et attentionnée.
+        Rejoignez notre famille de parents et d'enfants, et découvrez la tranquillité d'esprit que nous pouvons vous offrir.</p>
     </div>
     <div class="mamcontainer">
-        <img src="" alt="">
-        <p></p>
+        <img src="<?php echo base_url('./images/Claudie1.png'); ?>" alt="breeeef">
+        <p>Claudie Daffe</p>
     </div>
 </div>
