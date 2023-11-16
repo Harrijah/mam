6202ce43254ec1d32dbd9c0ae260ebad102c43a2
Hari <andrianarivohari@gmail.com>
2023-11-16 15:45:10 +0200

    font

--- app/Views/template-parts/header.php
+++ app/Views/template-parts/header.php
@@ -3,6 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <!-- <link href="https://fonts.googleapis.com/css2?family=Mali&display=swap" rel="stylesheet"> -->
+    <link href="https://fonts.googleapis.com/css2?family=Delius+Swash+Caps&family=Mali&display=swap" rel="stylesheet">
+    
     <link rel="stylesheet" href="<?php echo base_url('./style/css/bootstrap.css'); ?>">
     <link rel="stylesheet" href="<?php echo base_url('./style/css/bootstrap.min.css'); ?>">
     <link rel="stylesheet" href="<?php echo base_url('./font-awesome/font-awesome4/css/font-awesome.css'); ?>">
@@ -10,6 +15,7 @@
     <link rel="stylesheet" href="<?php echo base_url('./font-awesome/font-awesome5/css/all.css'); ?>">
     <link rel="stylesheet" href="<?php echo base_url('./font-awesome/font-awesome5/css/all.min.css'); ?>">
     <link rel="stylesheet" href="<?php echo base_url('./style/index.css'); ?>">
+
     
 
 
