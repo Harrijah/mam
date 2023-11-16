e163816c78fc37e030b4ccfbf30faecab93bdaa1
Hari <andrianarivohari@gmail.com>
2023-11-16 20:39:04 +0200

    footer

--- app/Views/template-parts/footer.php
+++ app/Views/template-parts/footer.php
@@ -1,4 +1,52 @@
 
+    <div class="mamfooter">
+        <div class="footercontainer">
+            <div class="footcol01">
+                <!-- <img src="<?php echo base_url('./images/sleeping.jpg'); ?>" alt=""> -->            
+                <div class="svg-container02">
+                    <svg width="100%" height="auto" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 600 400">
+                    
+                        <defs>
+                            <!-- Création d'un chemin pour le découpage -->
+                            <clipPath id="clip-path3">
+                                <path transform="translate(0.000000,373.000000) scale(0.100000,-0.100000)"   d="M762 3619 c-254 -32 -500 -217 -582 -436 -52 -140 -51 -128 -50 -753 0 -395 -5 -679 -15 -875 -31 -598 -14 -797 83 -995 52 -108 157 -211 277 -272 158 -81 310 -122 570 -157 308 -41 401 -43 1375 -40 514 2 1072 0 1240 -5 422 -12 552 3 729 85 124 58 198 121 280 239 73 105 118 209 150 350 43 187 46 266 46 1275 l0 960 -23 75 c-93 299 -365 520 -677 549 -94 9 -351 1 -640 -20 -246 -19 -923 -17 -1395 3 -456 19 -1272 30 -1368 17z"/>
+                            </clipPath>
+                        </defs>
+                        <!-- Utilisation du clipPath pour découper l'image -->
+                        <image href="<?php echo base_url('./images/sleeping.jpg'); ?>" width="100%" height="100%" clip-path="url(#clip-path3)" />
+                    </svg>
+                </div>
+            </div>
+            <div class="footcol02">
+                <h3>Liens utiles</h3>
+                <ul>
+                    <li><a href="">A propos de Tiazaza</a></li>
+                    <li><a href="">Créer un compte</a></li>
+                    <li><a href="">Formulaire de pré-admission</a></li>
+                    <li><a href="">Nous contacter</a></li>
+                    <li><a href="https://www.facebook.com/mamtiazaza/"><i class="fab fa-facebook display-1 fontawe"></i></a></li>
+                </ul>
+            </div>
+            <div class="footcol03">
+                <h3>Nos coordonnées</h3>
+                <ul>
+                    <li><b>Adresse</b> : 101 Avenue des Mourlingues 31130 Balma</li>
+                    <li><b>Horaires</b> : 
+                        <ul>
+                            <li>Lundi - Vendredi : 7h30 - 19h00</li>
+                            <li>Fermé le week-end et les jours fériés</li>
+                        </ul>
+                    </li>
+                    <li><b>Mail</b> : mamtiazaza[at]gmail.com</li>
+                    <li><b>Téléphone</b> : 07  81 25 56 40</li>
+                </ul>
+
+            </div>
+
+        </div>
+    </div>
+
+    
     <script type="text/javascript" src="<?php echo base_url('./style/js/all.min.js') ?>"></script>
     <script type="text/javascript" src="<?php echo base_url('./style/js/all.js') ?>"></script>
     <script type="text/javascript" src="<?php echo base_url('./style/js/jquery.min.js') ?>"></script>
