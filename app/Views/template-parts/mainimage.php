4f2d2540aef5e43bbe919f63dcb10b9489294539
Hari <andrianarivohari@gmail.com>
2023-11-17 15:18:39 +0200

    nouveau design

--- app/Views/template-parts/mainimage.php
+++ app/Views/template-parts/mainimage.php
@@ -1,50 +1,24 @@
 <div class="fullwidth">
     <div class="ppltitle">
         <div class="titles">
-            <h1><span> Tiazaza </span>Maison d' Assistants Maternels</h1>
-            <h2>"Bien grandir, naturellement"</h2>
+            <img src="<?php echo base_url('./images/logo.png'); ?>" alt="mon logo">
+            <h1>MAISON D'ASSISTANTS MATERNELS</h1>
+            <h2>Bien grandir, naturellement</h2>
         </div>
     </div>
-    <div class="pplimage">
-        <div class="svg-container">
-            <svg width="100%" height="auto" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 480">
-                <defs>
-                    <!-- Création d'un chemin pour le découpage -->
-                    <clipPath id="clip-path">
-                        <path d="M490 130c3.59 -7.12 5.61 -14.9 5.61 -23.02c0 -35.25 -37.69 -63.86 -84.15 -63.86c-17.27 0 -33.40 3.99 -46.72 10.78c-24.28 -31.93 -69.60 -53.35 -121.58 -53.35c-77.49 0 -140.25 47.63 -140.25 106.43c0 1.80 0.09 3.59 0.18 5.39c-49.09 13.10 -84.33 48.62 -84.33 90.40c0 52.88 56.54 95.79 126.23 95.79l322.57 0c61.97 0 112.20 -38.11 112.20 -85.14c0 -41.17 -38.57 -75.56 -89.76 -83.41Z"/>
-                    </clipPath>
-                </defs>
-                <!-- Utilisation du clipPath pour découper l'image -->
-                <image href="<?php echo base_url('./images/cuddly.jpg'); ?>" width="100%" height="100%" clip-path="url(#clip-path)" />
-            </svg>
-        </div>
-    </div>
-    <img src="<?php echo base_url('./images/bird2.gif'); ?>" alt="oiseau qui vole" class="mybird">
-    
+    <div class="pplimage"></div>
 </div>
 
 <section class="normalcontainer">
+    <h2>Bienvenue à la MAM <span>Tiazaza</span></h2>
     <span class="anim01"></span>
     <span class="anim02"></span>
 
     <div class="bandeau01">
-        <div class="imagepart">            
-            <div class="svg-container02">
-                <svg width="100%" height="auto" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 530 370">
-                
-                    <defs>
-                        <!-- Création d'un chemin pour le découpage -->
-                        <clipPath id="clip-path2">
-                            <path transform="translate(0.000000,373.000000) scale(0.100000,-0.100000)" d="M762 3619 c-254 -32 -500 -217 -582 -436 -52 -140 -51 -128 -50 -753 0 -395 -5 -679 -15 -875 -31 -598 -14 -797 83 -995 52 -108 157 -211 277 -272 158 -81 310 -122 570 -157 308 -41 401 -43 1375 -40 514 2 1072 0 1240 -5 422 -12 552 3 729 85 124 58 198 121 280 239 73 105 118 209 150 350 43 187 46 266 46 1275 l0 960 -23 75 c-93 299 -365 520 -677 549 -94 9 -351 1 -640 -20 -246 -19 -923 -17 -1395 3 -456 19 -1272 30 -1368 17z"/>
-                        </clipPath>
-                    </defs>
-                    <!-- Utilisation du clipPath pour découper l'image -->
-                    <image href="<?php echo base_url('./images/pere_enfant.jpg'); ?>" width="100%" height="100%" clip-path="url(#clip-path2)" />
-                </svg>
-            </div>
+        <div class="imagepart">
+            <img src="<?php echo base_url('./images/pere_enfant.jpg'); ?>" alt="">
         </div>
         <div class="textpart">
-            <h2>Bienvenue à la MAM <span>Tiazaza</span></h2>
             <p>
             Nous comprenons à quel point il est important de trouver un environnement chaleureux et sécurisé pour votre enfant, où il peut grandir, apprendre et s'épanouir. Chez Tiazaza, nous mettons tout en œuvre pour offrir à votre enfant une deuxième maison, où il se sentira aimé, encouragé et entouré de soins attentionnés.
             <br /><br />
@@ -57,17 +31,18 @@
     <div class="pictocontainer">
         <div class="pictobox">
             <h3>Bien-être</h3>
-            <svg xmlns="http://www.w3.org/2000/svg" id="Layer_1" data-name="Layer 1" width="512" height="512" viewBox="0 0 24 24">
-                <path d="M9.5,11c-.276,0-.5-.224-.5-.5,0-1.215-.771-2.5-1.5-2.5s-1.5,1.285-1.5,2.5c0,.276-.224,.5-.5,.5s-.5-.224-.5-.5c0-1.652,1.069-3.5,2.5-3.5s2.5,1.848,2.5,3.5c0,.276-.224,.5-.5,.5Zm9.5-.5c0-1.652-1.069-3.5-2.5-3.5s-2.5,1.848-2.5,3.5c0,.276,.224,.5,.5,.5s.5-.224,.5-.5c0-1.215,.771-2.5,1.5-2.5s1.5,1.285,1.5,2.5c0,.276,.224,.5,.5,.5s.5-.224,.5-.5Zm5,1.5C24,5.383,18.617,0,12,0S0,5.383,0,12s5.383,12,12,12,12-5.383,12-12Zm-1,0c0,6.065-4.935,11-11,11S1,18.065,1,12,5.935,1,12,1s11,4.935,11,11Zm-5.185,3.888c.214-.174,.246-.488,.073-.702-.174-.213-.489-.246-.703-.074-.023,.019-2.37,1.889-5.186,1.889s-5.162-1.87-5.186-1.889c-.216-.172-.528-.139-.703,.074-.173,.214-.141,.528,.073,.702,.106,.087,2.638,2.112,5.815,2.112s5.709-2.025,5.815-2.112Z"/>
-            </svg>
+            <img src="<?php echo base_url('./images/mere.png'); ?>" alt="">
+
         </div>
         <div class="pictobox">
             <h3>Jeux</h3>
-            <svg xmlns="http://www.w3.org/2000/svg" id="Layer_1" data-name="Layer 1" width="512" height="512" viewBox="0 0 24 24"><path d="M23.666,10.418l-1.008-3.025c-.477-1.431-1.811-2.393-3.32-2.393h-2.366c-.25-2.247-2.16-4-4.472-4H4.5C2.019,1,0,3.019,0,5.5v14.5c0,1.654,1.346,3,3,3,1.043,0,1.962-.535,2.5-1.344,.538,.809,1.457,1.344,2.5,1.344,1.654,0,3-1.346,3-3v-1h5v1c0,1.654,1.346,3,3,3s3-1.346,3-3v-1.338c1.181-.563,2-1.769,2-3.162v-3.026c0-.7-.112-1.392-.334-2.056Zm-1.957-2.708l1.008,3.025c.029,.088,.056,.177,.081,.266h-5.799V6h2.338c1.078,0,2.031,.687,2.372,1.709ZM1,5.5c0-1.93,1.57-3.5,3.5-3.5H12.5c1.93,0,3.5,1.57,3.5,3.5v12.5H1V5.5ZM3,22c-1.103,0-2-.897-2-2v-1H5v1c0,1.103-.897,2-2,2Zm7-2c0,1.103-.897,2-2,2s-2-.897-2-2v-1h4v1Zm11,0c0,1.103-.897,2-2,2s-2-.897-2-2v-1h3.5c.17,0,.337-.012,.5-.036v1.036Zm2-4.5c0,1.378-1.122,2.5-2.5,2.5h-3.5v-6h5.979c.014,.157,.021,.315,.021,.474v3.026Z"/></svg>
+            <img src="<?php echo base_url('./images/toys.png'); ?>" alt="">
+            
         </div>
         <div class="pictobox">
             <h3>Soins</h3>
-            <svg xmlns="http://www.w3.org/2000/svg" id="Layer_1" data-name="Layer 1" width="512" height="512" viewBox="0 0 24 24"><path d="M15.5,0c-.603,0-1.5,.399-1.5,1.5v7.5H4.735l-1.39-1.668c-.706-.847-1.743-1.332-2.845-1.332-.276,0-.5,.224-.5,.5s.224,.5,.5,.5c.805,0,1.562,.354,2.076,.973l1.424,1.709v.819c0,2.481,2.019,4.5,4.5,4.5h4.432l-3.488,4.186c-.292-.119-.61-.186-.944-.186-1.378,0-2.5,1.122-2.5,2.5s1.122,2.5,2.5,2.5,2.5-1.122,2.5-2.5c0-.683-.275-1.302-.721-1.754l3.721-4.465,3.721,4.465c-.445,.452-.721,1.071-.721,1.754,0,1.378,1.122,2.5,2.5,2.5s2.5-1.122,2.5-2.5-1.122-2.5-2.5-2.5c-.334,0-.653,.066-.944,.186l-3.488-4.186h4.432c2.481,0,4.5-2.019,4.5-4.5v-1C24,.108,15.585,0,15.5,0Zm-7,23c-.827,0-1.5-.673-1.5-1.5s.673-1.5,1.5-1.5,1.5,.673,1.5,1.5-.673,1.5-1.5,1.5Zm12.5-1.5c0,.827-.673,1.5-1.5,1.5s-1.5-.673-1.5-1.5,.673-1.5,1.5-1.5,1.5,.673,1.5,1.5ZM15,1.5c0-.403,.275-.494,.499-.5,.3,0,7.211,.118,7.492,8h-7.991V1.5Zm8,9c0,1.93-1.57,3.5-3.5,3.5H8.5c-1.93,0-3.5-1.57-3.5-3.5v-.5H23v.5Z"/></svg>
+            <img src="<?php echo base_url('./images/landau.png'); ?>" alt="">
+            
         </div>
     </div>
 
