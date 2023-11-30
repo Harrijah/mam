849444bb4d22d6af02781c0fff52259f910ef613
Hari <andrianarivohari@gmail.com>
2023-11-30 11:40:10 +0200

    hihihi

--- public/js/main.js
+++ public/js/main.js
@@ -19,28 +19,28 @@ window.addEventListener('scroll', () => {
         menu.style.top = 0;
     }
 
-    if (part1) {
-        if (scrollValue > 0.40 && scrollValue < 0.54) {
-            part1.style.setProperty('--defaultpath', "76% 24% 90% 10% / 20% 85% 15% 80%");
-        } else {
-            part1.style.setProperty('--defaultpath', "5px");
-        }
-    }
-    if (part2) {
+    // if (part1) {
+    //     if (scrollValue > 0.40 && scrollValue < 0.54) {
+    //         part1.style.setProperty('--defaultpath', "76% 24% 90% 10% / 20% 85% 15% 80%");
+    //     } else {
+    //         part1.style.setProperty('--defaultpath', "5px");
+    //     }
+    // }
+    // if (part2) {
         
-        if (scrollValue > 0.42 && scrollValue < 0.6) {
-            part2.style.setProperty('--defaultscale', "1");
-        } else {
-            part2.style.setProperty('--defaultscale', "0");
-        }
-    }
+    //     if (scrollValue > 0.42 && scrollValue < 0.6) {
+    //         part2.style.setProperty('--defaultscale', "1");
+    //     } else {
+    //         part2.style.setProperty('--defaultscale', "0");
+    //     }
+    // }
     
-    if (scrollValue > 0.92) {
-        imgfooter.style.borderRadius = "76% 24% 90% 10% / 20% 85% 15% 80%";
-        footcol01.style.setProperty('--defaultopacity', '1');
-    } else {
-        imgfooter.style.borderRadius = "5px";
-        footcol01.style.setProperty('--defaultopacity', '0');
-    }
+    // if (scrollValue > 0.92) {
+    //     imgfooter.style.borderRadius = "76% 24% 90% 10% / 20% 85% 15% 80%";
+    //     footcol01.style.setProperty('--defaultopacity', '1');
+    // } else {
+    //     imgfooter.style.borderRadius = "5px";
+    //     footcol01.style.setProperty('--defaultopacity', '0');
+    // }
 
 });
